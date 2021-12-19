41827b0292dc709fca4ddc30f31a0636be1a4cbe
Aly Baez <alybaez@Alys-MacBook-Pro.local>
2021-12-18 18:09:12 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkmode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extent: {},
+  },
+  plugins: [],
+};
